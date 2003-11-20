736fe4759832443770034eb782ee94201d4e5b4e
Dimitri Sokolyuk <demon@dim13.org>
2003-11-20 14:07:32 +0000

    clenup

--- watch.c
+++ watch.c
@@ -2,32 +2,18 @@
 /* watch - execute program periodicaly, showing output fullscreen */
 /*
  * Copyright (c) 2003 demon <demon@vhost.dymdns.org>
- * All rights reserved.
- *   
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
  *
- * - Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- * - Neither the name of Demon nor the names of his contributors may
- *   be used to endorse or promote products derived from this software
- *   without specific prior written permission.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED  TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <err.h>
@@ -39,8 +25,8 @@
 #include <curses.h>
 #define MAXBUF 255
 
-static char copyright[] = "Copyright (C) 2003 demon <demon@vhost.dyndns.org>";
-static char version[] = "$Revision$";
+static char copyright[] = "(c) 2003 demon <demon@vhost.dyndns.org>";
+static char version[] = "0.2";
 extern char *__progname;
 time_t tval;
 int die_flag;
@@ -63,7 +49,7 @@ int main (int argc, char *argv[]) {
     while ((ch = getopt(argc, argv, "s:v")) != -1)
 	switch (ch) {
 	    case 'v':
-		(void)fprintf(stderr, "%s %s\n%s\n", __progname, version, copyright);
+		(void)fprintf(stderr, "%s %s %s\n", __progname, version, copyright);
 		exit(1);
 		break;
 	    case 's':
